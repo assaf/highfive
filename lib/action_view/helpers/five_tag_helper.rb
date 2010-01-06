210dc477221f75fe457c8a0f0a95d9240df7731c
Assaf Arkin <assaf@labnotes.org>
2010-01-05 23:12:38 -0800

    Added FormBuilder methods for new HTML 5 input types

--- lib/action_view/helpers/five_tag_helper.rb
+++ lib/action_view/helpers/five_tag_helper.rb
@@ -1,66 +1,30 @@
 module ActionView
   module Helpers
     module FiveTagHelper
-      # Input field with type email.
-      def email_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "email"))
+      %w(email url number range search color date month week time datetime datetime_local).each do |type|
+        class_eval <<-EOS, __FILE__, __LINE__
+          def #{type}_field(object_name, method, options = {})
+            InstanceTag.new(object_name, method, self, options.delete(:object)).to_input_field_tag("#{type.gsub(/_/, '-')}", options)
+          end
+          def #{type}_field_tag(name, value = nil, options = {})
+            text_field_tag(name, value, options.stringify_keys.update("type" => "#{type.gsub(/_/, '-')}"))
+          end
+        EOS
       end
 
-      # Input field with type url.
-      def url_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "url"))
+      def self.included(base)
+        %w(email url number range search color date month week time datetime datetime_local).each do |type|
+          FormBuilder.class_eval <<-EOS, __FILE__, __LINE__
+            def #{type}_field(method, options = {}) # def email_field(method, options = {})
+              @template.send(                       #   @template.send(
+                "#{type}_field",                      #     "email_field",
+                @object_name,                       #     @object_name,
+                method,                             #     method,
+                objectify_options(options))         #     objectify_options(options))
+            end                                     # end
+          EOS
+        end
       end
-
-      # Input field with type number.
-      def number_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "number"))
-      end
-
-      # Input field with type range.
-      def range_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "range"))
-      end
-
-      # Input field with type search.
-      def search_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "search"))
-      end
-
-      # Input field with type color.
-      def color_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "color"))
-      end
-
-      # Input field with type date.
-      def date_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "date"))
-      end
-
-      # Input field with type month.
-      def month_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "month"))
-      end
-
-      # Input field with type week.
-      def week_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "week"))
-      end
-
-      # Input field with type time.
-      def time_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "time"))
-      end
-
-      # Input field with type datetime.
-      def datetime_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "datetime"))
-      end
-
-      # Input field with type datetime-local.
-      def datetime_local_field_tag(name, value = nil, options = {})
-        text_field_tag(name, value, options.stringify_keys.update("type" => "datetime-local"))
-      end
-      
     end
   end
 end
