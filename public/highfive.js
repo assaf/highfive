a5654391fa77b05e87148ebc26093edbf47d7e3a
Assaf Arkin <assaf@labnotes.org>
2010-01-08 12:20:43 -0800

    Fixed: under Chrome (possibly other browsers), jQuery DELETE/PUT confuse Rails (or just don't work), so using _method instead.

--- public/highfive.js
+++ public/highfive.js
@@ -1,40 +1,45 @@
 (function($) {
-  $.error = function(exception) { console.log(exception) }
-
   // Spooky action at a distance (link_to_remote).
   $("a[data-remote]").live("remote", function() {
-    var link = $(this);
-    var question = link.attr("data-confirm");
+    var link = $(this),
+        method = (link.attr("data-method") || "get").toLowerCase(),
+        question = link.attr("data-confirm"),
+        success = link.attr("data-success"),
+        error = link.attr("data-failure"),
+        options = {};
     if (question && !confirm(question)) return false;
-    options = {};
-    options.type = link.attr("data-method");
     options.url = link.attr("href"); 
+    if (method == "get" || method == "post") { options.type = method }
+    else { options.type = "post" ; options.data = { _method: method } }
     options.element = link;
-    var success = link.attr("data-success");
     if (success) options.success = function(response) { $("#" + success).html(response) }
-    else options.success = function(response) { try { eval(response) } catch(ex) { $.error(ex) } }
-    var error = link.attr("data-failure");
+    else options.success = function(response) { try { eval(response) } catch(ex) { console.log(ex) } }
     if (error) options.error = function(response) { $("#" + error).html(response) };
     $.ajax(options);
   }).live("click", function() { $(this).trigger("remote"); return false });
 
   // Spooky action at a distance (form_remote_tag).
   $("form[data-remote]").live("remote", function() {
-    var form = $(this);
-    var question = form.attr("data-confirm");
+  try {
+    var form = $(this),
+        method = (form.attr("data-method") || "post").toLowerCase(),
+        question = form.attr("data-confirm"),
+        success = form.attr("data-success"),
+        error = form.attr("data-failure"),
+        buttons = form.find(":submit:enabled,button:enabled"),
+        options = {};
     if (question && !confirm(question)) return false;
-    options = {};
-    options.type = form.attr("method");
     options.url = form.attr("action"); 
     options.data = form.serialize();
+    if (method == "get" || method == "post") { options.type = method }
+    else { options.type = "post" ; options.data = options.data + "&_method=" + method }
     options.element = form;
-    var success = form.attr("data-success");
     if (success) options.success = function(response) { $("#" + success).html(response) }
-    else options.success = function(response) { try { eval(response) } catch(ex) { $.error(ex) } }
-    var error = form.attr("data-failure");
+    else options.success = function(response) { try { eval(response) } catch(ex) { console.log(ex) } }
     if (error) options.error = function(response) { $("#" + error).html(response) };
-    var buttons = form.find(":submit:enabled,button:enabled").attr("disabled", "disabled");
+    buttons.attr("disabled", "disabled");
     options.complete = function(status) { buttons.attr("disabled", null) }
     $.ajax(options);
+    } catch (ex) { alert(ex) }
   })
 }(jQuery))
