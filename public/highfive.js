193be883140e933ce05ad8b2467eba1fd7c84c00
Assaf Arkin <assaf@labnotes.org>
2010-01-08 17:12:21 -0800

    Added: trigger success event handler on link/form after evaluating response. Fixed: Chrome likes to send the request as XML, which fails when there's no body (e.g. links that post).

--- public/highfive.js
+++ public/highfive.js
@@ -1,4 +1,8 @@
 (function($) {
+  // Chrome likes to send the request as XML, which fails when there's no body
+  // (e.g. links that post).
+  var setContentType = function(xhr) { xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded") }
+
   // Spooky action at a distance (link_to_remote).
   $("a[data-remote]").live("remote", function() {
     var link = $(this),
@@ -6,14 +10,14 @@
         question = link.attr("data-confirm"),
         success = link.attr("data-success"),
         error = link.attr("data-failure"),
-        options = {};
+        options = { beforeSend: setContentType };
     if (question && !confirm(question)) return false;
     options.url = link.attr("href"); 
     if (method == "get" || method == "post") { options.type = method }
     else { options.type = "post" ; options.data = { _method: method } }
     options.element = link;
     if (success) options.success = function(response) { $("#" + success).html(response) }
-    else options.success = function(response) { try { eval(response) } catch(ex) { console.log(ex) } }
+    else options.success = function(response) { try { eval(response) ; $(link).trigger("success", [response]) } catch(ex) { console.log(ex) } }
     if (error) options.error = function(response) { $("#" + error).html(response) };
     $.ajax(options);
   }).live("click", function() { $(this).trigger("remote"); return false });
@@ -27,7 +31,7 @@
         success = form.attr("data-success"),
         error = form.attr("data-failure"),
         buttons = form.find(":submit:enabled,button:enabled"),
-        options = {};
+        options = { beforeSend: setContentType };
     if (question && !confirm(question)) return false;
     options.url = form.attr("action"); 
     options.data = form.serialize();
@@ -35,7 +39,7 @@
     else { options.type = "post" ; options.data = options.data + "&_method=" + method }
     options.element = form;
     if (success) options.success = function(response) { $("#" + success).html(response) }
-    else options.success = function(response) { try { eval(response) } catch(ex) { console.log(ex) } }
+    else options.success = function(response) { try { eval(response) ; $(form).trigger("success", [response]) } catch(ex) { console.log(ex) } }
     if (error) options.error = function(response) { $("#" + error).html(response) };
     buttons.attr("disabled", "disabled");
     options.complete = function(status) { buttons.attr("disabled", null) }
