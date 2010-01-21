59d762a262ecad907b14d6985a30559a0a9f19d4
Assaf Arkin <assaf@labnotes.org>
2010-01-20 19:33:52 -0800

    link_to options confirm, popup and method now handled using unobtrusive JS. To use the method option, you must have authenticity token available from the window object, like this:    <script>window.AUTH_TOKEN="<%= escape_javascript form_authenticity_token %>"</script>

--- lib/action_view/helpers/unobtrusive.rb
+++ lib/action_view/helpers/unobtrusive.rb
@@ -46,6 +46,16 @@ module ActionView
 
         form_tag(html.delete(:action) || url_for(url), html, &block)
       end
+
+    private
+      def convert_options_to_javascript!(html_options, url = '')
+        html_options["data-confirm"] = html_options.delete("confirm")
+        html_options["data-popup"] = html_options.delete("popup")
+        if method = html_options.delete("method")
+          html_options["data-method"] = method
+          html_options["onclick"] = "return false"
+        end
+      end
     end
   end
 end
--- public/highfive.js
+++ public/highfive.js
@@ -1,73 +1,76 @@
 (function($) {
+  // Confirmation before processing link.
+  $("a[data-confirm]").live("click", function() { return confirm($(this).attr("data-confirm")) });
+  // Popup a new page.
+  $("a[data-popup]").live("click", function() { window.open($(this).attr("href"), $(this).attr("data-popup")) ; return false });
   // Spooky action at a distance (link_to_remote).
-  $("a[data-remote]").live("remote", function() {
+  $("a[data-remote]").live("click", function() {
     var link = $(this),
         method = (link.attr("data-method") || "get").toLowerCase(),
-        question = link.attr("data-confirm"),
         success = link.attr("data-success"),
         error = link.attr("data-failure");
         options = { contentType: "application/x-www-form-urlencoded" };
-    if (!question || confirm(question)) {
-      options.url = link.attr("href"); 
-      if (method == "get" || method == "post")
-        options.type = method
-      else {
-        options.type = "post"
-        options.data = { _method: method }
-      }
-      options.element = link;
-      options.dataType = success ? "html" : "script";
-      options.success = success ?
-        function(response)              { $(document.getElementById(success)).html(response) } :
-        function(response, status, xhr) { link.trigger("success", [response, status, xhr]) }
-      options.error = error ?
-        function(xhr, status, ex) { $(document.getElementById(error)).html(xhr.responseText) } :
-        function(xhr, status, ex) { link.trigger("failure", [xhr, status, ex]) }
-      $.ajax(options);
+    options.url = link.attr("href"); 
+    if (method == "get" || method == "post")
+      options.type = method
+    else {
+      options.type = "post"
+      options.data = { _method: method }
     }
+    options.element = link;
+    options.dataType = success ? "html" : "script";
+    options.success = success ?
+      function(response)              { $(document.getElementById(success)).html(response) } :
+      function(response, status, xhr) { link.trigger("success", [response, status, xhr]) }
+    options.error = error ?
+      function(xhr, status, ex) { $(document.getElementById(error)).html(xhr.responseText) } :
+      function(xhr, status, ex) { link.trigger("failure", [xhr, status, ex]) }
+    $.ajax(options);
     return false
-  }).live("click", function() { $(this).trigger("remote"); return false });
-
-  // Spooky action at a distance (form_remote_tag).
-  $("form[data-remote]").live("remote", function() {
-    var form = $(this),
-        method = (form.attr("data-method") || "post").toLowerCase(),
-        question = form.attr("data-confirm"),
-        success = form.attr("data-success"),
-        error = form.attr("data-failure"),
-        buttons = form.find(":submit:enabled,button:enabled"),
-        options = { contentType: "application/x-www-form-urlencoded" };
-    if (!question || confirm(question)) {
-      options.url = form.attr("action"); 
-      options.data = form.serialize();
-      if (method == "get" || method == "post")
-        options.type = method
-      else {
-        options.type = "post"
-        options.data = options.data + "&_method=" + method
-      }
-      options.element = form;
-      options.dataType = success ? "html" : "script";
-      options.success = success ?
-        function(response)              { $(document.getElementById(success)).html(response) } :
-        function(response, status, xhr) { form.trigger("success", [response, status, xhr]) }
-      options.error = error ?
-        function(xhr, status, ex) { $(document.getElementById(error)).html(xhr.responseText) } :
-        function(xhr, status, ex) { form.trigger("failure", [xhr, status, ex]) }
-      buttons.attr("disabled", "disabled");
-      options.complete = function() { buttons.attr("disabled", null) }
-      $.ajax(options);
+  });
+  // Links that use forms for various methods (e.g. signup: delete)
+  $("a[data-method]").live("click", function() {
+    var link = $(this), method = link.attr("data-method") || "get",
+        form = $("<form>", { style: "display:none", method: method == "get" ? "get" : "post", action: link.attr("href") });
+    if (method != "get") {
+      form.append($("<input>", { type: "hidden", name: "authenticity_token", value: window.AUTH_TOKEN }));
+      if (method != "post") form.append($("<input>", { type: "hidden", name: "_method", value: method }));
     }
-    return false
-  }).live("submit", function() { $(this).trigger("remote"); return false });
-
-  // Links that are actually forms: looks like a link but no auto-follow.
-  $("a[href]").live("submit", function() {
-    var link = $(this),
-        method = link.attr("method") || "get",
-        form = $("<form>", { style: "display:none", method: method, action: link.attr("href") });
     $(document.body).append(form);
     form.submit();
     return false
   });
+
+  // Confirmation before submitting form.
+  $("form[data-confirm]").live("submit", function() { return confirm($(this).attr("data-confirm")) });
+  // Spooky action at a distance (form_remote_tag).
+  $("form[data-remote]").live("submit", function() {
+    var form = $(this),
+        method = (form.attr("data-method") || "post").toLowerCase(),
+        success = form.attr("data-success"),
+        error = form.attr("data-failure"),
+        buttons = form.find(":submit:enabled,button:enabled"),
+        options = { contentType: "application/x-www-form-urlencoded" };
+    options.url = form.attr("action"); 
+    options.data = form.serialize();
+    if (method == "get" || method == "post")
+      options.type = method
+    else {
+      options.type = "post"
+      options.data = options.data + "&_method=" + method
+    }
+    options.element = form;
+    options.dataType = success ? "html" : "script";
+    options.success = success ?
+      function(response)              { $(document.getElementById(success)).html(response) } :
+      function(response, status, xhr) { form.trigger("success", [response, status, xhr]) }
+    options.error = error ?
+      function(xhr, status, ex) { $(document.getElementById(error)).html(xhr.responseText) } :
+      function(xhr, status, ex) { form.trigger("failure", [xhr, status, ex]) }
+    buttons.attr("disabled", "disabled");
+    options.complete = function() { buttons.attr("disabled", null) }
+    $.ajax(options);
+    return false
+  }).live("submit", function() { $(this).trigger("remote"); return false });
+
 }(jQuery))
